326968d130befb7497e654fce7c6ddf1f3c0da67
Blake Pfaff <blake.a.pfaff2@gmail.com>
2020-09-08 11:58:52 -0500

    adds search by name and search all to API and starts home index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,10 +6,11 @@
     <!-- <h2>{{ this.cards.cards[0] }}</h2> -->
 
     <br />
-    <input v-on:keyup.enter="fetchCards()" type="text" v-model="name" placeholder="edit me" />
+    <input v-on:keyup.enter="fetchCards()" type="text" v-model="name" placeholder="Search All" />
     <div v-for="card in cards.cards" :key="card.index">
       <div>{{ card.name }}</div>
       <div>{{ card.text }}</div>
+      <img :src="`${card.imageUrl}`" alt />
     </div>
   </div>
 </template>
