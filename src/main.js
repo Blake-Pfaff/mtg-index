cad6ba4f04570c849959b2036732922057b5543f
Blake Pfaff <blake.a.pfaff2@gmail.com>
2020-09-21 11:54:55 -0500

    adds page pangnation styles

--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
+// Add this to your imports at the top
+import Paginate from "vuejs-paginate";
+// Install the component as a global component
+Vue.component("paginate", Paginate);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
